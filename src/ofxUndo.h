5d274399f752b59a938b2d6a11b8376fbc2be741
nariakiiwatani <nariakiiwatani@annolab.com>
2018-08-10 09:59:21 +0900

    simplify

--- src/ofxUndo.h
+++ src/ofxUndo.h
@@ -1,52 +1,28 @@
 #pragma once
 
+#include "ofEvents.h"
+
 namespace ofx { namespace undo {
 
-template<typename Context, typename Object=Context>
+template<typename Context, typename History=std::vector<Context>>
 class Manager
 {
 public:
-	void store() {
-		if(last_action_ == REDO) {
-			redo2undo();
-		}
-		undo_history_.push(static_cast<Context*>(this)->createUndo());
-		redo_history_ = std::stack<Object>();
-		last_action_ = STORE;
-	}
-	void clear() {
-		undo_history_ = std::stack<Object>();
-		redo_history_ = std::stack<Object>();
-		last_action_ = CLEAR;
-	}
-	bool undo() {
-		if(!canUndo()) return false;
-		if(last_action_ == UNDO && undo_history_.size() > 1) {
-			undo2redo();
-		}
-		auto &o = undo_history_.top();
-		static_cast<Context*>(this)->loadUndo(o);
-		notify(o);
-		last_action_ = UNDO;
-		return true;
-	}
-	bool redo() {
-		if(!canRedo()) return false;
-		if(last_action_ == REDO && redo_history_.size() > 1) {
-			redo2undo();
-		}
-		auto &o = redo_history_.top();
-		static_cast<Context*>(this)->loadUndo(o);
-		notify(o);
-		last_action_ = REDO;
-		return true;
-	}
-	bool canUndo() const { return !undo_history_.empty(); }
-	bool canRedo() const { return !redo_history_.empty(); }
-	ofEvent<Object>& restoreEvent() { return restore_event_; }
+	void store(const Context &c);
+	void clear();
+	Context& undo();
+	Context& redo();
+	bool canUndo() const;
+	bool canRedo() const;
+	ofEvent<Context>& restoreEvent() { return restore_event_; }
 protected:
-	ofEvent<Object> restore_event_;
-	std::stack<Object> undo_history_, redo_history_;
+	History history_;
+	std::size_t current_index_ = 0;
+	ofEvent<Context> restore_event_;
+
+	void notify(Context &c) { ofNotifyEvent(restore_event_, c); }
+	int getUndoLength() const { return current_index_; }
+	int getRedoLength() const { return history_.size()-current_index_; }
 	enum Action {
 		STORE,
 		UNDO,
@@ -54,21 +30,57 @@ protected:
 		CLEAR
 	};
 	Action last_action_=CLEAR;
-	
-	void undo2redo() {
-		auto o = undo_history_.top();
-		undo_history_.pop();
-		redo_history_.push(o);
-	}
-	void redo2undo() {
-		auto o = redo_history_.top();
-		redo_history_.pop();
-		undo_history_.push(o);
-	}
-	void notify(Object &o) {
-		ofNotifyEvent(restore_event_, o);
-	}
 };
+
+template<typename Context, typename History>
+void Manager<Context, History>::store(const Context &c)
+{
+	if(last_action_ == REDO) {
+		++current_index_;
+	}
+	history_.resize(current_index_++);
+	history_.push_back(c);
+	last_action_ = STORE;
+}
+template<typename Context, typename History>
+void Manager<Context, History>::clear()
+{
+	history_.clear();
+	current_index_ = 0;
+	last_action_ = CLEAR;
+}
+template<typename Context, typename History>
+Context& Manager<Context, History>::undo()
+{
+	if(last_action_ == UNDO) {
+		--current_index_;
+	}
+	Context &c = history_.at(current_index_-1);
+	notify(c);
+	last_action_ = UNDO;
+	return c;
+}
+template<typename Context, typename History>
+Context& Manager<Context, History>::redo()
+{
+	if(last_action_ == REDO) {
+		++current_index_;
+	}
+	Context &c = history_.at(current_index_);
+	notify(c);
+	last_action_ = REDO;
+	return c;
+}
+template<typename Context, typename History>
+bool Manager<Context, History>::canUndo() const
+{
+	return getUndoLength() > (last_action_==UNDO?1:0);
+}
+template<typename Context, typename History>
+bool Manager<Context, History>::canRedo() const
+{
+	return getRedoLength() > (last_action_==REDO?1:0);
+}
 }}
-template<typename Context, typename Object=Context>
-using ofxUndo = ofx::undo::Manager<Context, Object>;
+template<typename Context, typename History=std::vector<Context>>
+using ofxUndo = ofx::undo::Manager<Context, History>;
